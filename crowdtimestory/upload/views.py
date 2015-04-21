4b46b1fdb16a980b3a01f32c80409785bcd1c3af
Albert Yang <ayoungbest@gmail.com>
2015-04-21 16:17:46 -0400

    removed redundant jquery file

--- crowdtimestory/upload/views.py
+++ crowdtimestory/upload/views.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-    abort, render_template, Blueprint, sqlite3
+    abort, render_template, Blueprint 
 import sys, os
 from werkzeug import secure_filename
 import re
